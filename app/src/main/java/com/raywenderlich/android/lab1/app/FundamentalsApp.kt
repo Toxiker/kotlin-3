95268e6d3e0fc81a57a1cf0585a6fc1ea26fab71
Toxiker <alex10040156@gmail.com>
2024-03-18 18:13:50 +0300

    Added all the changes
    Добавил все изменения

--- app/src/main/java/com/raywenderlich/android/lab1/app/FundamentalsApp.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/app/FundamentalsApp.kt
@@ -1,5 +1,4 @@
 package com.raywenderlich.android.lab1.app
-
 import androidx.compose.animation.Crossfade
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -7,7 +6,6 @@ import androidx.compose.runtime.Composable
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
 import com.raywenderlich.android.lab1.router.Screen
 import com.raywenderlich.android.lab1.screens.*
-
 @Composable
 fun FundamentalsApp() {
     Surface(color = MaterialTheme.colors.background) {
@@ -19,6 +17,11 @@ fun FundamentalsApp() {
                 is Screen.Buttons -> ExploreButtonsScreen()
                 is Screen.ProgressIndicator -> ProgressIndicatorScreen()
                 is Screen.AlertDialog -> AlertDialogScreen()
+                is Screen.Row -> RowScreen()
+                is Screen.Column -> ColumnScreen()
+                is Screen.Box -> BoxScreen()
+                is Screen.Surface -> SurfaceScreen()
+                is Screen.Scaffold -> ScaffoldScreen()
             }
         }
     }
